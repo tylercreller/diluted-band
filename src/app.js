1eaa75bf508d0b73438cfe512c2743be658a14e5
Tyler Creller <tylercreller@gmail.com>
2018-12-05 18:59:59 -0500

    Add normalize

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Background from './components/Background/Background';
 import BodyContent from './components/BodyContent/BodyContent';
+import 'normalize.css/normalize.css';
 import './shared/styles/_base.scss';
 
 const Layout = () => (
