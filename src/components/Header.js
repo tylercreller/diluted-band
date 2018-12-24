74d61d0dd6aa06faf54378f8bc1e8b31a8d867b2
Tyler Creller <tylercreller@gmail.com>
2018-12-24 13:20:44 -0500

    Mobile considerations - nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,36 @@
 import React from 'react';
 
-const Header = () => (
-	<div>
-		<div className="container">
-			<div className="header">
-				<div className="header__logo-container">
-					<img
-						className="header__logo-container__logo"
-						src={'images/DiludedLogo.png'}
-					/>
-				</div>
-				<div className="header__nav-container">
-					<a href="https://dilutedband.bandcamp.com/merch/">MERCH</a>
-					<a href="#shows">SHOWS</a>
-					<a href="#social">SOCIAL</a>
-					<a href="mailto:dilutedband1@gmail.com" target="_blank">
-						CONTACT
-					</a>
+export default class Header extends React.Component {
+	toggleNav = () => {
+		const nav = document.querySelector('.header__nav-container');
+		nav.classList.toggle('header__nav-container--toggle-show');
+	};
+
+	render() {
+		return (
+			<div>
+				<div className="container">
+					<div className="header">
+						<div className="header__logo-container">
+							<img
+								className="header__logo-container__logo"
+								src={'images/DiludedLogo.png'}
+							/>
+						</div>
+						<div className="header__nav-toggle" onClick={this.toggleNav}>
+							<i className="fa fa-bars" />
+						</div>
+						<div className="header__nav-container">
+							<a href="https://dilutedband.bandcamp.com/merch/">MERCH</a>
+							<a href="#shows">SHOWS</a>
+							<a href="#social">SOCIAL</a>
+							<a href="mailto:dilutedband1@gmail.com" target="_blank">
+								CONTACT
+							</a>
+						</div>
+					</div>
 				</div>
 			</div>
-		</div>
-	</div>
-);
-
-export default Header;
+		);
+	}
+}
