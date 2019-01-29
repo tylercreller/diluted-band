6b8921ca0f2cf68382388546d2eb1a790c6b0a6a
tylercreller <tylercreller@gmail.com>
2019-01-28 19:10:51 -0500

    Pretty up social markup

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,8 +1,7 @@
 import React from 'react';
 
 export default class Social extends React.Component {
-
-	navigationEvent = (label) => {
+	navigationEvent = label => {
 		gtag('event', 'click', {
 			event_category: 'Social Navigation',
 			event_label: label
@@ -16,30 +15,46 @@ export default class Social extends React.Component {
 					<div id="social" className="social">
 						<h1 className="section-header">Follow Us</h1>
 						<div className="social__icons-container">
-							<a href="https://www.instagram.com/dilutedband/" target="_blank" onClick={this.navigationEvent.bind(this, 'Instagram')}>
+							<a
+								href="https://www.instagram.com/dilutedband/"
+								target="_blank"
+								onClick={this.navigationEvent.bind(this, 'Instagram')}
+							>
 								<div className="social__icons-container__icon">
 									<i className="fab fa-instagram fa-5x" />
 								</div>
 							</a>
-							<a href="https://www.facebook.com/DilutedBand" target="_blank" onClick={this.navigationEvent.bind(this, 'Facebook')}>
+							<a
+								href="https://www.facebook.com/DilutedBand"
+								target="_blank"
+								onClick={this.navigationEvent.bind(this, 'Facebook')}
+							>
 								<div className="social__icons-container__icon">
 									<i className="fab fa-facebook-f fa-5x" />
 								</div>
 							</a>
-							<a href="https://twitter.com/dilutedband" target="_blank" onClick={this.navigationEvent.bind(this, 'Twitter')}>
+							<a
+								href="https://twitter.com/dilutedband"
+								target="_blank"
+								onClick={this.navigationEvent.bind(this, 'Twitter')}
+							>
 								<div className="social__icons-container__icon">
 									<i className="fab fa-twitter fa-5x" />
 								</div>
 							</a>
-							<a href="https://soundcloud.com/dilutedband" target="_blank" onClick={this.navigationEvent.bind(this, 'Soundcloud')}>
+							<a
+								href="https://soundcloud.com/dilutedband"
+								target="_blank"
+								onClick={this.navigationEvent.bind(this, 'Soundcloud')}
+							>
 								<div className="social__icons-container__icon">
 									<i className="fab fa-soundcloud fa-5x" />
 								</div>
 							</a>
 						</div>
 					</div>
-				</div >
-			</React.Fragment >
+				</div>
+			</React.Fragment>
 		);
 	}
 }
