bd0617c555297a3cd4c2aea989c5fd4fc2a1862c
Tyler Creller <tylercreller@gmail.com>
2020-01-09 12:13:13 -0500

    Toggle overlay off

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,7 +7,7 @@ import Overlay from './Overlay';
 
 export default class Layout extends React.Component {
 	state = {
-		showMessage: true
+		showMessage: false
 	};
 	hideMessage = () => {
 		document.getElementById('body').classList.remove('noscroll');
